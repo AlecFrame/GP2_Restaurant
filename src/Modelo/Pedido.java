253c33f92796274b0efddea57dc10300a339be91
AlecFrame <walteravertacnik@sanluis.edu.ar>
2024-10-09 19:41:37 -0300

    Pedido vacio
    Pedido vacio

---
+++ src/Modelo/Pedido.java
@@ -0,0 +1,17 @@
+
+package Modelo;
+
+public class Pedido {
+    private int idPedido;
+    private int dni_mesero;
+    private int numero_mesa;
+    private double pago;
+    private boolean pendiente;
+
+    public Pedido() {
+    }
+    
+    
+    
+    
+}
