70f93ec2829c99e02cdfc10fd8de492f15b0a54d
PatPagnone <muralismo.sanluis@gmail.com>
2024-10-14 17:33:06 -0300

    Cambios vista ppal fondo

--- src/Vistas/main.java
+++ src/Vistas/main.java
@@ -4,7 +4,8 @@ package Vistas;
 public class main {
 
     public static void main(String[] args) {
-        
+        VentPrincipal v1 = new VentPrincipal();
+        v1.setVisible(true);
     }
     
 }
